e06ae33d8be1b50238e38edc6c87815c0f3a086c
ProfesorA15 <147123026+ProfesorA15@users.noreply.github.com>
2023-11-03 09:25:59 +0700

    Add files via upload

---
+++ tipe data string.py
@@ -0,0 +1,13 @@
+nama_depan = "Qodrat"
+nama_belakang = "Fadhila"
+nama_lengkap = nama_depan + ' ' + nama_belakang
+usia = '12'
+alamat = 'Bangka'
+kata_mutiara = "Don't forget smile"
+
+print(nama_lengkap, '('+usia+')', ',', 'dari', alamat, ',kata mutiara:', kata_mutiara)
+
+print('\nTipe dari nama_lengkap:', type(nama_lengkap))
+print('Tipe dari usia:', type(usia))
+print('Tipe dari alamat:', type(alamat))
+print('Tipe dari kata_mutiara:', type(kata_mutiara))
